185725d49ce8066fe50da6c7ded5863cf29aefbf
xiaoyan <14077535+2986577461@users.noreply.github.com>
2026-07-20 23:36:57 +0800

    修改循环为10次

--- src/main/java/com/xiaoyan/Main.java
+++ src/main/java/com/xiaoyan/Main.java
@@ -8,7 +8,8 @@ public class Main {
         // 查看 IntelliJ IDEA 建议如何修正。
         System.out.print("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        //10次，我不管
+        for (int i = 1; i <= 10; i++) {
             //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
             // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
             System.out.println("i = " + i);
