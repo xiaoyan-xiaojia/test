84a02daca85781958a11ecb2605994b1bf78f355
test <xiaoyan2986577461@outlook.com>
2026-07-21 14:10:07 +0800

    added a annotation named noArgsConstructor

--- src/main/java/com/xiaoyan/Student.java
+++ src/main/java/com/xiaoyan/Student.java
@@ -2,12 +2,14 @@ package com.xiaoyan;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Student {
     String name;
     Integer age;
