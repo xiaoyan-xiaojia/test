3ab5bdd3c38e053f5f87f855913f1f2c71f65cd6
test <xiaoyan2986577461@outlook.com>
2026-07-21 13:41:16 +0800

    added a class named Student.java

--- src/main/java/com/xiaoyan/Main.java
+++ src/main/java/com/xiaoyan/Main.java
@@ -2,9 +2,7 @@ package com.xiaoyan;
 
 public class Main {
     public static void main(String[] args) {
-        Student s=new Student();
-        s.setAge(30);
-        s.setName("damin");
+        Student s=new Student("xiaomin",23);
         System.out.println(s);
     }
 }
